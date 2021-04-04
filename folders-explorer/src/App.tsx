b61b6357405b4d32a84b20393377e2f0b95d9e3e
AmirEppel <amireppel@gmail.com>
2021-04-04 21:55:24 +0300

    full folder name and switch

--- folders-explorer/src/App.tsx
+++ folders-explorer/src/App.tsx
@@ -52,17 +52,45 @@ function App() {
       let idNoSpaces = item.id.replace(/\s/g, '').split(':')
       folderBase = insertToTree(folderBase, idNoSpaces, { lastUpdated: item.lastUpdated })
     })
-  
+
     setFolder(folderBase)
   },
     [])
+  let i = true
+  let folderStruct = '';
+  let currentFolder = '';
+  let parentFoldersArray = []
+  if (fileFolder !== null) {
+
+    let parentFolder = fileFolder.parentFolder
+    while (i !== false) {
+
+      if (parentFolder !== null) {
+
+        parentFoldersArray.unshift(parentFolder)
+        parentFolder = parentFolder.parentFolder
+
+      }
+      else {
+        i = false
+      }
+
+
+    }
+  }
   return (
     <div className="App">
-      {fileFolder !== null ? <div>
-        <div className="App-Header">Your folder explorer:</div>
-        {fileFolder.parentFolder !== null ? (<div className="Back-Arrow" onClick={() => setFolder(fileFolder.parentFolder)}>&#8629;</div>) : <div className="Root-Folder">Root folder</div>}
+       {fileFolder !== null ? <div>
+        <div className="App-Header">Folder Explorer</div>
+        {fileFolder.parentFolder !== null ? (<div className="Back-Arrow" onClick={() => setFolder(fileFolder.parentFolder)}>&#8629;</div>) : <div className="Root-Folder"></div>}
         <div className="Folder-Name-Title">Folder name:</div>
-        <div className="Folder-Name"> {fileFolder.displayName} </div>
+        <div className="Folder-Name">
+        {parentFoldersArray.map((item, index) => {
+                     return <span key={index} className="Parent-Folder" onClick={() =>setFolder(item) }
+             >{item.displayName}/</span>
+          })}
+          <span className="Current-Folder">{fileFolder.displayName}</span>
+        </div>
         {(fileFolder.folderContent !== null && fileFolder.folderContent.length > 0) ?
           <div className="Folder-Content">Content:
           <ul>
@@ -70,12 +98,12 @@ function App() {
                 let fileContent = item.fileContent
                 if (fileContent === null) {
                   return <li key={index} className="Sub-Folder" onClick={() => {
-                    {setFolder(item)}
+                    { setFolder(item) }
                   }} >{item.displayName}</li>
                 }
                 else {
-                  return <li key={index} className="File" ><span className="File-Content-Header">File name: </span><span>{item.displayName},</span> <span 
-                  className="File-Content-Header">updated at: </span>
+                  return <li key={index} className="File" ><span className="File-Content-Header">File name: </span><span>{item.displayName},</span> <span
+                    className="File-Content-Header">updated at: </span>
                     <span >{fileContent.lastUpdated.updatedAt},</span>
                     <span className="File-Content-Header">updated by: </span><span>{fileContent.lastUpdated.updatedBy}</span></li>
                 }
